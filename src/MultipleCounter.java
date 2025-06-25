893720fab9197e8918a17212576b25506fdf42e1
Suresh <nagasureshchakali@gmail.com>
2025-06-25 12:44:26 +0530

    25/06/25

---
+++ src/MultipleCounter.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MultipleCounter {
+
+	public static void main(String[] args) {
+        int[] input = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 1; i <= 9; i++) {
+            result.put(i, 0);
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            int count = 0;
+            for (int num : input) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+            result.put(i, count);
+        }
+        System.out.print("{");
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            System.out.print(entry.getKey() + ": " + entry.getValue());
+            if (index < result.size() - 1) {
+                System.out.print(", ");
+            }
+            index++;
+        }
+        System.out.println("}");
+    }
+
+}
---
+++ src/OddSeriesGenerator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OddSeriesGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number (a): ");
+        int a = sc.nextInt();
+        System.out.print("Output: ");
+        for (int i = 1; i <= a; i++) {
+            int odd = 2 * i - 1;
+            System.out.print(odd);
+            if (i != a) {
+                System.out.print(", ");
+            }
+        }
+
+        sc.close();
+    }
+}
